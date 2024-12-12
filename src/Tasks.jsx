811761d7092ff61ca4287e2a37512d822c8b629b
KIRAN KUMHAR <kiran.kumar91@live.com>
2024-12-13 00:05:42 +0530

    task  completed event

--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -5,27 +5,46 @@ import { useState } from "react";
 const Tasks = () => {
     const [tasks, setTasks] = useState([]);
 
-
     useEffect(() => {
+        fetchTasks();
+
+    }, []);
+    
+    const fetchTasks = () => {
         axios.get('http://127.0.0.1:8000/api/tasks-all')
-        .then(response => {
-            setTasks(response.data);
-        })
-        .catch(error =>{
-            console.error("There was an error fetching the task!", error);
-        })
-    },[]);
+        .then(response => setTasks(response.data))
+        .catch(error => console.error("There was an error fetching the task!", error))
+    };
+
+    const markAsCompleted = (id) => {
+        axios.post(`http://127.0.0.1:8000/api/tasks-all/completed/${id}`)
+            .then(() => {
+                alert("Task marked as completed!");
+                fetchTasks(); // Refresh the task list
+            })
+            .catch(error => console.error("Error marking task as completed:", error));
+    };
 
     return(
         <div>
             <h1>Task List</h1>
             <ul>
                 {tasks.map(task => (
-                    <li key={task.id}> {task.title}</li>
+                    <li key={task.id}> 
+                    {task.id}
+                    {task.title}
+                    {task.completed ? (
+                        <span style={{ color: 'green', marginLeft: '10px' }}>Completed</span>
+                    ) : (
+                        <button onClick={() => markAsCompleted(task.id)} style={{ marginLeft: '10px' }}>
+                                Mark as Completed
+                            </button>
+                    )}
+                    </li>
                 ))}
             </ul>
         </div>
-    )
+    );
         
 } ;
 
